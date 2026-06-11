f1d81ce58c23e40d10349353398b389c15b75922
Sebastian <se.li@maxlindner.com>
2026-06-11 17:02:43 +0200

    Refactor variable names for clarity and consistency in DataFrame handling

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ file_get = [file for file in os.listdir(path)]
 print(file_get)
 
 df0 = pd.read_csv(file_get[0])
-dfSuicideDet = pd.read_csv(file_get[1])
+df1 = pd.read_csv(file_get[1])
 
-print(df0.head())
-print(dfSuicideDet.head())
+print(df0.head())       #df0 enthält Combined Data.csv
+print(df1.head())       #df1 enthält Suicide_detection.csv
 
 # cleanup und merging der beiden Dataframes, falls kompatibel
 
 # Unique Werte in den Dataframes überprüfen, bzw Welche Werte sich Doppeln
 # Und ob das für die Klassifikation relevant sein könnte, oder ob es sich um Rauschen handelt, welches entfernt werden sollte
 print(df0.nunique())
-print(dfSuicideDet.nunique())
+print(df1.nunique())
 
 # Klassifikation zum finden, oder testen von Features, welche relevant sein könnten
