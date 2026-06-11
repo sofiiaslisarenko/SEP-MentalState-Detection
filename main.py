78ea293131a69b9b1d95731ba080f4b15b324bc2
Sebastian <se.li@maxlindner.com>
2026-06-11 16:24:05 +0200

    two dataset loading

--- main.py
+++ main.py
@@ -11,18 +11,6 @@ os.chdir(path)
 # Erstellen einer Liste mit den csv Dateien und erste Auswertungen
 file_get = [file for file in os.listdir(path)]
 print(file_get)
-for sets in file_get:
-    print("=" * 80)
-    print("FILE:", os.path.basename(sets))
 
-    df = pd.read_csv(sets)
-
-    # Show columns first
-    print("\nColumns:", df.columns.tolist())
-
-    # Try to show value counts for ALL text-like columns
-    text_cols = df.select_dtypes(include=["object"]).columns
-
-    for col in text_cols:
-        print(f"\n🔹 Value counts for column: '{col}'")
-        print(df[col].value_counts())
+df0 = pd.read(file_get[0])
+dfSuicideDet = pd.read(file_get[1])
