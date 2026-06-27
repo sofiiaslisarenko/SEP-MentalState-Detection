8350ff218c604516f4b9caaea4dd10534f0d6411
olramai <olra.maiboroda@gmail.com>
2026-06-27 13:51:56 +0200

    Aufgeteilt in zwei Funktionen: clean_data(): jetzt nur mit einem df return print_clean_data(): enthält alle Prints, die davor in clean_data() waren.

--- data_clean.py
+++ data_clean.py
@@ -1,8 +1,7 @@
 from data_loading import load_data_kaggle
 
-
+# In zwei Funktionen aufgeteilt zum Bereinigen und zur Ausgabe
 def clean_data():
-
     df = load_data_kaggle()
 
     def remove_non_ascii(text):
@@ -12,34 +11,38 @@ def clean_data():
             return text
         return text.encode("ascii", errors="ignore").decode("ascii")
 
-    df["statement"] = df["statement"].apply(remove_non_ascii) # Entfernt alle Nicht-ASCII-Zeichen
+    df["statement"] = df["statement"].apply(remove_non_ascii)  # Entfernt alle Nicht-ASCII-Zeichen
 
-    mask = df["statement"].str.contains("ð", na=False) # Überprüfen des Filters für nicht-ASCII Zeichen
+    df = df.dropna(subset=['statement', 'status'])
+
+    # Entfernung von 13 Spalten mit NAME die leer sind
+    df = df[df['statement'] != '#NAME?']
+
+    return df
+
+def print_clean_data():
+
+    df = load_data_kaggle() # Der Datensatz vor Bereinigung
+    df_clean = clean_data()
+
+    mask = df_clean["statement"].str.contains("ð", na=False) # Überprüfen des Filters für nicht-ASCII Zeichen
     print("Mojibake-Treffer nach Filter:", mask.sum())
 
     # 1 Typen von Daten in jeder Spalte
     print("Datentypen:")
-    print(df.info())
+    print(df_clean.info())
     # 2 Gibt es leere Spalte
     print("Leere Columns:")
-    print(df.isnull().sum())
+    print(df_clean.isnull().sum())
     # 3 Gibt es Reihen, die wiederholt wurden
-    print("Anzahl von Duplikaten:", df.duplicated().sum())
+    print("Anzahl von Duplikaten:", df_clean.duplicated().sum())
     # 4 Unique values werden statt 'status' die echte Name der Spalte
-    print("Unique Classes:", df['status'].unique())
-    print(df['status'].value_counts())
+    print("Unique Classes:", df_clean['status'].unique())
+    print(df_clean['status'].value_counts())
     # 5 Die leere Spalten werden gelöscht
     print("Groesse bevor Cleaning:", df.shape)
-    df = df.dropna(subset=['statement', 'status'])
-    print("Groesse nach Cleaning:", df.shape)
+    print("Groesse nach Cleaning:", df_clean.shape)
 
-    # Entfernung von 13 Spalten mit NAME die leer sind
-    df = df[df['statement'] != '#NAME?']
-    print("Groesse mit NAME:", df.shape)
-    print(df[df['statement'].str.contains('NAME', na=False)]['statement'].tolist())
-
-
-    return df
 
 if __name__ == "__main__":
     df = clean_data()
